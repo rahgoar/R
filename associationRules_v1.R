3cd3820f5c140920f37f619aab906d033f0ed03c
Ari <rahgoar@users.noreply.github.com>
2019-10-27 16:44:45 -0400

    Add files via upload
    https://towardsdatascience.com/association-rule-mining-in-r-ddf2d044ae50
    https://cran.r-project.org/web/packages/arules/arules.pdf

---
+++ associationRules_v1.R
@@ -0,0 +1,26 @@
+library(arules)
+library(arulesViz)
+data("Groceries")
+class(Groceries)
+inspect(head(Groceries, 12))
+
+grocery_rules <- apriori(Groceries, parameter = list(support = 0.01, confidence = 0.5))
+inspect(head(sort(grocery_rules, by = "confidence"), 3))
+
+#Shows what products are bought before buying "whole milk" and will generate rules that lead to buying "whole milk".
+#Limiting the number of rules generated
+wholemilk_rules <- apriori(data=Groceries, parameter=list (supp=0.001,conf = 0.08), appearance = list (rhs="whole milk"))
+
+
+# This generates only one rule in the output.
+grocery_rules_increased_support <- apriori(Groceries, parameter = list(support = 0.02, confidence = 0.5))
+
+
+subsets <- which(colSums(is.subset(grocery_rules, grocery_rules)) > 1)
+grocery_rules <- grocery_rules[-subsets]
+
+# This gives more than 1,500,000 rules
+rules <- apriori(Groceries, parameter = list(supp = 0.0001, conf = 0.5))
+# This gives 982,000 rules.
+rules_chi2 <- apriori(Groceries, parameter = list(supp = 0.0001, conf = 0.5, arem = "chi2"))
+
---
+++ visualRules.R
@@ -0,0 +1,9 @@
+## S3 method for class 'rules'
+plot(wholemilk_rules, method = NULL, measure = "support", shading = "lift", 
+     interactive = NULL, engine = "default", data = NULL, control = NULL)
+## S3 method for class 'itemsets'
+plot(wholemilk_rules, method = NULL, measure = "support", shading = NA,
+     interactive = NULL, engine = "default", data = NULL, control = NULL)
+
+
+plot(wholemilk_rules, method = "graph", control = list(verbose = TRUE))
