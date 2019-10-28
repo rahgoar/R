358ea02846312706b4c95de3bc02089bfd736918
Ari <rahgoar@users.noreply.github.com>
2019-10-27 20:59:51 -0400

    Update MyFirst_KNN.R

--- MyFirst_KNN.R
+++ MyFirst_KNN.R
@@ -41,3 +41,5 @@ result
 result$byClass["Precision"]
 result$byClass["Recall"]
 result$byClass["F1"]
+
+#Ref https://towardsdatascience.com/k-nearest-neighbors-algorithm-with-examples-in-r-simply-explained-knn-1f2c88da405c
